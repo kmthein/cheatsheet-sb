7d6cd55c1037c735c935f5cbe5f07b7a49e88224
kmthein <kmthein96.mm@gmail.com>
2024-08-29 06:34:14 +0630

    project setup

---
+++ src/main/java/com/cheatsheet/CheatsheetApplication.java
@@ -0,0 +1,13 @@
+package com.cheatsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheatsheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheatsheetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cheatsheet/CheatsheetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheatsheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheatsheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
